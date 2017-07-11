0327664e6c9c80b1988fe9a9dbc7563522457833
Corey Griffin <iamcoreyg@gmail.com>
2017-07-11 14:29:10 -0700

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import '../css/app.scss';
 import $ from "jquery";
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import 'waypoints';
+// import '../node_modules/bootstrap/dist/js/bootstrap.min.js'; // uncomment if needed
+// import 'waypoints'; // uncomment if needed
 
 $(function() {
    
